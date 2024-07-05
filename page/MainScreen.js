131c207a3d10b4dfbd93a6fe0cef6cf4c4038f92
김형훈 <kimhyeonghun@gimhyeonghun-ui-MacBookAir.local>
2024-07-05 09:53:15 +0900

    data add

--- page/MainScreen.js
+++ page/MainScreen.js
@@ -6,6 +6,7 @@ const data = [
   { date: '2024-07-01', description: '점심식사', amount: '4400원' },
   { date: '2024-07-03', description: '저녁식사', amount: '4600원' },
   { date: '2024-07-04', description: '점심식사', amount: '4800원' },
+  { date: '2024-07-04', description: '저녁 식재료 구입', amount: '3800원' },
   // 데이터 추가
 ];
 
